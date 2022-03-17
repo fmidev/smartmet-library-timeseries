e4f3102a82a93204dbc78d3a56419fc55ce28fae
Andris Pavēnis <andris.pavenis@fmi.fi>
2022-03-17 08:35:06 +0200

    Reorder includes

--- timeseries/TimeSeriesInclude.h
+++ timeseries/TimeSeriesInclude.h
@@ -1,11 +1,11 @@
 #pragma once
 
+#include <spine/None.h>
+#include <spine/LonLat.h>
 #include "TimeSeriesAggregator.h"
 #include "TimeSeriesOutput.h"
 #include "TimeSeriesUtility.h"
 #include "TimeSeriesGeneratorCache.h"
 #include "DataFilter.h"
-#include <spine/None.h>
-#include <spine/LonLat.h>
 
 namespace TS = SmartMet::TimeSeries;
