a7ef40ee1a331ba18dfc7524b965446eead9cda5
Andris Pavēnis <andris.pavenis@fmi.fi>
2025-01-31 09:26:13 +0200

    Revert "TimeParameters: add descriptions of part of parameters"
    This reverts commit 1292044b98b3345819bb3301b361fcbc9d4f3244.

--- timeseries/TimeParameters.cpp
+++ timeseries/TimeParameters.cpp
@@ -82,8 +82,7 @@ TimeParameters::TimeParameters()
           auto seconds = st.daylength().total_seconds();
           int minutes = Fmi::numeric_cast<int>(round(static_cast<double>(seconds) / 60.0));
           return Value(minutes);
-        },
-        "Return the length of the day in minutes");
+        });
 
     add (EPOCHTIME_PARAM,
         [](TimeParameterArgs& args)
@@ -98,15 +97,13 @@ TimeParameters::TimeParameters()
         {
             const int hour = args.ldt.local_time().time_of_day().hours();
             return Value(hour);
-        },
-        "Return the hour of the day (0-23)");
+        });
 
     add (ISOTIME_PARAM,
         [](TimeParameterArgs& args)
         {
             return Value(Fmi::to_iso_string(args.ldt.local_time()));
-        },
-        "Return the time in ISO format");
+        });
 
     add (LOCALTIME_PARAM,
         [](TimeParameterArgs& args)
@@ -122,16 +119,14 @@ TimeParameters::TimeParameters()
         {
             const std::string ret = Fmi::format_time(args.outlocale, "%b", args.ldt);
             return Value(ret);
-        },
-        "Return the month abbreviation");
+        });
 
     add (MONTH_PARAM,
         [](TimeParameterArgs& args)
         {
             const std::string ret = Fmi::format_time(args.outlocale, "%B", args.ldt);
             return Value(ret);
-        },
-        "Return the month name");
+        });
 
     add (MOONDOWN24H_PARAM,
         [](TimeParameterArgs& args)
@@ -141,15 +136,14 @@ TimeParameters::TimeParameters()
             const auto ret = Fmi::to_string(!lt.moonrise_today() && !lt.moonset_today() &&
                                             !lt.above_horizont_24h());
             return Value(ret);
-        },
-        "Return 1 if the moon is below the horizon for the whole day or 0 otherwise");
+        });
 
     add (MOONPHASE_PARAM,
         [](TimeParameterArgs& args)
         {
             const double result = Fmi::Astronomy::moonphase(args.ldt.utc_time());
             return Value(result);
-        });
+       });
 
     add (MOONRISE_PARAM,
         [](TimeParameterArgs& args)
@@ -159,8 +153,7 @@ TimeParameters::TimeParameters()
             if (lt.moonrise_today())
                 return Value(args.timeformatter.format(lt.moonrise.local_time()));
             return Value();
-        },
-        "Return the time of moonrise or an empty string if the moon does not rise");
+        });
 
     add (MOONRISE2_PARAM,
         [](TimeParameterArgs& args)
@@ -170,8 +163,7 @@ TimeParameters::TimeParameters()
             if (lt.moonrise2_today())
                 return Value(args.timeformatter.format(lt.moonrise2.local_time()));
             return Value();
-        }
-        , "Return the time of second moonrise in deay or an empty string if the moon does not rise second time");
+        });
 
     add (MOONRISE2TODAY_PARAM,
         [](TimeParameterArgs& args)
@@ -179,8 +171,7 @@ TimeParameters::TimeParameters()
             Fmi::Astronomy::lunar_time_t lt =
                 Fmi::Astronomy::lunar_time(args.ldt, args.loc.longitude, args.loc.latitude);
             return Value(Fmi::to_string(lt.moonrise2_today()));
-        },
-        "Return 1 if the moon rises second time today or 0 otherwise");
+        });
 
     add (MOONRISETODAY_PARAM,
         [](TimeParameterArgs& args)
@@ -188,8 +179,7 @@ TimeParameters::TimeParameters()
             Fmi::Astronomy::lunar_time_t lt =
                 Fmi::Astronomy::lunar_time(args.ldt, args.loc.longitude, args.loc.latitude);
             return Value(Fmi::to_string(lt.moonrise_today()));
-        },
-        "Return 1 if the moon rises today or 0 otherwise");
+        });
 
     add (MOONSET_PARAM,
         [](TimeParameterArgs& args)
@@ -199,8 +189,7 @@ TimeParameters::TimeParameters()
             if (lt.moonset_today())
                 return Value(args.timeformatter.format(lt.moonset.local_time()));
             return Value();
-        },
-        "Return the time of moonset or an empty string if the moon does not set");
+        });
 
     add (MOONSET2_PARAM,
         [](TimeParameterArgs& args)
@@ -210,8 +199,7 @@ TimeParameters::TimeParameters()
             if (lt.moonset2_today())
                 return Value(args.timeformatter.format(lt.moonset2.local_time()));
             return Value();
-        },
-        "Return the time of second moonset in deay or an empty string if the moon does not set second time");
+        });
 
     add (MOONSET2TODAY_PARAM,
         [](TimeParameterArgs& args)
@@ -219,8 +207,7 @@ TimeParameters::TimeParameters()
             Fmi::Astronomy::lunar_time_t lt =
                 Fmi::Astronomy::lunar_time(args.ldt, args.loc.longitude, args.loc.latitude);
             return Value(Fmi::to_string(lt.moonset2_today()));
-        },
-        "Return 1 if the moon sets second time today or 0 otherwise");
+        });
 
     add (MOONSETTODAY_PARAM,
         [](TimeParameterArgs& args)
@@ -228,8 +215,7 @@ TimeParameters::TimeParameters()
             Fmi::Astronomy::lunar_time_t lt =
                 Fmi::Astronomy::lunar_time(args.ldt, args.loc.longitude, args.loc.latitude);
             return Value(Fmi::to_string(lt.moonset_today()));
-        },
-        "Return 1 if the moon sets today or 0 otherwise");
+        });
 
     add (MOONUP24H_PARAM,
         [](TimeParameterArgs& args)
@@ -237,8 +223,7 @@ TimeParameters::TimeParameters()
             Fmi::Astronomy::lunar_time_t lt =
                 Fmi::Astronomy::lunar_time(args.ldt, args.loc.longitude, args.loc.latitude);
             return Value(Fmi::to_string(lt.above_horizont_24h()));
-        },
-        "Return 1 if the moon is above the horizon for the whole day or 0 otherwise");
+        });
 
     add (NOON_PARAM,
         [](TimeParameterArgs& args)
@@ -246,8 +231,7 @@ TimeParameters::TimeParameters()
             Fmi::Astronomy::solar_time_t st =
                 Fmi::Astronomy::solar_time(args.ldt, args.loc.longitude, args.loc.latitude);
             return Value(args.timeformatter.format(st.noon.local_time()));
-        },
-        "Return the time of noon");
+        });
 
     add (ORIGINTIME_PARAM,
         [](TimeParameterArgs& args)
@@ -263,8 +247,7 @@ TimeParameters::TimeParameters()
             Fmi::Astronomy::solar_position_t sp =
                 Fmi::Astronomy::solar_position(args.ldt, args.loc.longitude, args.loc.latitude);
             return Value(sp.azimuth);
-        },
-        "Return the azimuth of the sun");
+        });
 
     add (SUNDECLINATION_PARAM,
         [](TimeParameterArgs& args)
@@ -272,8 +255,7 @@ TimeParameters::TimeParameters()
             Fmi::Astronomy::solar_position_t sp =
                 Fmi::Astronomy::solar_position(args.ldt, args.loc.longitude, args.loc.latitude);
             return Value(sp.declination);
-        },
-        "Return the declination of the sun");
+        });
 
     add (SUNELEVATION_PARAM,
         [](TimeParameterArgs& args)
@@ -281,8 +263,7 @@ TimeParameters::TimeParameters()
             Fmi::Astronomy::solar_position_t sp =
                 Fmi::Astronomy::solar_position(args.ldt, args.loc.longitude, args.loc.latitude);
             return Value(sp.elevation);
-        },
-        "Return the elevation of the sun");
+        });
 
     add (SUNRISE_PARAM,
         [](TimeParameterArgs& args)
@@ -290,8 +271,7 @@ TimeParameters::TimeParameters()
             Fmi::Astronomy::solar_time_t st =
                 Fmi::Astronomy::solar_time(args.ldt, args.loc.longitude, args.loc.latitude);
             return Value(args.timeformatter.format(st.sunrise.local_time()));
-        },
-        "Return the time of sunrise");
+        });
 
     add (SUNRISETODAY_PARAM,
         [](TimeParameterArgs& args)
@@ -299,8 +279,7 @@ TimeParameters::TimeParameters()
             Fmi::Astronomy::solar_time_t st =
                 Fmi::Astronomy::solar_time(args.ldt, args.loc.longitude, args.loc.latitude);
             return Value(Fmi::to_string(st.sunrise_today()));
-        },
-        "Return 1 if the sun rises today or 0 otherwise");
+        });
 
     add (SUNSET_PARAM,
         [](TimeParameterArgs& args)
@@ -308,8 +287,7 @@ TimeParameters::TimeParameters()
             Fmi::Astronomy::solar_time_t st =
                 Fmi::Astronomy::solar_time(args.ldt, args.loc.longitude, args.loc.latitude);
             return Value(args.timeformatter.format(st.sunset.local_time()));
-        },
-        "Return the time of sunset");
+        });
 
     add (SUNSETTODAY_PARAM,
         [](TimeParameterArgs& args)
@@ -317,8 +295,7 @@ TimeParameters::TimeParameters()
             Fmi::Astronomy::solar_time_t st =
                 Fmi::Astronomy::solar_time(args.ldt, args.loc.longitude, args.loc.latitude);
             return Value(Fmi::to_string(st.sunset_today()));
-        },
-        "Return 1 if the sun sets today or 0 otherwise");
+        });
 
     add (SUNUP24H_PARAM,
         [](TimeParameterArgs& args)
@@ -326,8 +303,7 @@ TimeParameters::TimeParameters()
             Fmi::Astronomy::solar_time_t st =
                 Fmi::Astronomy::solar_time(args.ldt, args.loc.longitude, args.loc.latitude);
             return Value(Fmi::to_string(st.polar_day()));
-        },
-        "Return 1 if the sun is up for the whole day (polar day) or 0 otherwise");
+        });
 
     add (SUNDOWN24H_PARAM,
         [](TimeParameterArgs& args)
@@ -335,8 +311,7 @@ TimeParameters::TimeParameters()
             Fmi::Astronomy::solar_time_t st =
                 Fmi::Astronomy::solar_time(args.ldt, args.loc.longitude, args.loc.latitude);
             return Value(Fmi::to_string(st.polar_night()));
-        },
-        "Return 1 if the sun is down for the whole day (polar night) or 0 otherwise");
+        });
 
     add (TIME_PARAM,
         [](TimeParameterArgs& args)
@@ -354,44 +329,38 @@ TimeParameters::TimeParameters()
         [](TimeParameterArgs& args)
         {
             return Value(args.timezone);
-        },
-        "Return the timezone");
+        });
 
     add (UTCTIME_PARAM,
         [](TimeParameterArgs& args)
         {
             return Value(args.timeformatter.format(args.ldt.utc_time()));
-        },
-        "Return the time in UTC");
+        });
 
     add (WDAY_PARAM,
         [](TimeParameterArgs& args)
         {
             return Value(Fmi::format_time(args.outlocale, "%a", args.ldt));
-        },
-        "Return the weekday abbreviation");
+        });
 
     add (WEEKDAY_PARAM,
         [](TimeParameterArgs& args)
         {
             return Value(Fmi::format_time(args.outlocale, "%A", args.ldt));
-        },
-        "Return the weekday name");
+        });
 
     add (XMLTIME_PARAM,
         [](TimeParameterArgs& args)
         {
             return Value(Fmi::to_iso_extended_string(args.ldt.local_time()));
-        },
-        "Return the time in XML format");
+        });
 
     add("date(format)", boost::regex("date\\(([^)]+)\\)"),
         [](const std::vector<std::string>& r_arg, TimeParameterArgs& args)
         {
             const std::string fmt = r_arg.at(0);
             return Value(Fmi::format_time(args.outlocale, fmt, args.ldt));
-        },
-        "Return the date in the specified format");
+        });
 }
 
 }  // namespace SpecialParameters
